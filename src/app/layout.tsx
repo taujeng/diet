c6f2bb98608533237e9d65c72a1d52e96d1c0a9f
Tau <taujeng.1@gmail.com>
2025-02-24 16:25:25 +0800

    feat: Add Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.png" />
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID!} />
       </head>
       <body>
